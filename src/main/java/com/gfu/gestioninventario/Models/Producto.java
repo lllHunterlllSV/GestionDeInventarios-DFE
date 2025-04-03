d3429aba21b2aaf23088e455f81b229d903d8406
Edwin Alberto <100868906+lllHunterlllSV@users.noreply.github.com>
2025-04-02 20:27:34 -0600

    Unificacion de carpetas

--- src/main/java/com/gfu/gestioninventario/Models/Producto.java
+++ src/main/java/com/gfu/gestioninventario/Models/Producto.java
@@ -1,4 +1,4 @@
-package Models;
+package com.gfu.gestioninventario.Models;
 
 import java.util.List;
 
@@ -11,7 +11,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
