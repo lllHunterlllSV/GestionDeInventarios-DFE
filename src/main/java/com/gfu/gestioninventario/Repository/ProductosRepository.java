f1f17a3527a7bc5452b21fabdd8829f021d13f7a
Tiffany Zaldaño <ia.tiffa100122@ufg.edu.sv>
2025-04-04 14:03:26 -0600

    Creacion de ProductoService y ProductoRepository

--- src/main/java/com/gfu/gestioninventario/Repository/ProductosRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gfu.gestioninventario.Repository;
-
-import com.gfu.gestioninventario.Models.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductosRepository extends JpaRepository<Producto, Integer> {
-}
