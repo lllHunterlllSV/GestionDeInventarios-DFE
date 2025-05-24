a21659b3d31c7c43c9cbc83aeaad1bb348ff0150
Adriana Fernandez <ia.adrianaf@ufg.edu.sv>
2025-05-24 09:19:13 -0600

    comentarios olvidados

--- src/main/java/com/gfu/gestioninventario/GestionInventarioApplication.java
+++ src/main/java/com/gfu/gestioninventario/GestionInventarioApplication.java
@@ -9,7 +9,7 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class
